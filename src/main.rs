3b9d0a201598b34fe59a541127d13a4908f9976f
Vladimír Ctibor <vladimir.malky@gmail.com>
2024-11-28 15:57:52 +0100

    Command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,42 @@
-use std::{env, fs};
+use std::fmt::Debug;
+use std::fs;
 use std::fs::{DirEntry, File};
 use std::path::Path;
 use std::io::Read;
 
-fn ls(path: Option<&Path>) {
-    let path = if let Some(path) = path {
-        path
-    } else {
-        &env::current_dir().unwrap()
-    };
+use clap::Parser;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Show directory entry statistics
+    #[arg(short = 'l')]
+    stats: bool,
+
+    /// Show hidden files (dot files)
+    #[arg(short)]
+    all: bool,
+
+    /// Path to directory or file
+    #[arg(index = 1, default_value = ".")]
+    path: String,
+}
+
+fn format_size(size: u64) -> String {
+    if size < 1024 {
+        format!("{}B", size)
+    } else if size < 1024 * 1024 {
+        format!("{}KB", size / 1024)
+    } else if size < 1024 * 1024 * 1024 {
+        format!("{}MB", size / 1024 / 1024)
+    } else if size < 1024 * 1024 * 1024 * 1024 {
+        format!("{}GB", size / 1024 * 1024 * 1024)
+    } else {
+        format!("{}TB", size / 1024 * 1024 * 1024 * 1024)
+    }
+}
+
+fn ls(path: &Path, args: &Args) {
     let entries = fs::read_dir(path).unwrap();
 
     let mut entries: Vec<DirEntry> = entries
@@ -21,30 +48,38 @@ fn ls(path: Option<&Path>) {
     for entry in entries {
         let filename= entry.file_name().into_string().unwrap();
 
-        println!("{}", filename);
+        if !args.all && filename.starts_with(".") {
+            continue;
+        }
+
+        if args.stats {
+            let metadata = entry.metadata().unwrap();
+            let len = format_size(metadata.len());
+
+            //let modified = metadata.modified().unwrap();
+            //let permissions = metadata.permissions().
+
+            println!("{} {}", len, filename);
+        } else {
+            println!("{}", filename);
+        }
     }
 
 }
 
-fn cat(path: &Path) {
+fn cat(path: &Path, args: &Args) {
     let mut file = File::open(path).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-
     println!("{}", contents);
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    if args.len() == 1 {
-        ls(None);
+    let args = Args::parse();
+    let path = Path::new(&args.path);
+    if path.is_dir() {
+        ls(path, &args);
     } else {
-        let arg = args.iter().nth(1).unwrap().clone();
-        let path = Path::new(&arg);
-        if path.is_dir() {
-            ls(Some(path));
-        } else {
-            cat(path);
-        }
+        cat(path, &args);
     }
 }
